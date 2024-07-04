64cea40d1f60f28712ef73937564262e7e88896a
Uday Kumar <38879020+UdayKumarKakani@users.noreply.github.com>
2024-07-04 20:10:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable client_secret {}
 variable ssh_public_key {}
 
 variable environment {
-    default = "dev"
+    default = "QA"
 }
 
 variable location {
@@ -17,13 +17,13 @@ variable node_count {
 
 
 variable dns_prefix {
-  default = "k8stest"
+  default = "k8stest01"
 }
 
 variable cluster_name {
-  default = "k8stest"
+  default = "k8stest01"
 }
 
 variable resource_group {
-  default = "kubernetes"
+  default = "kubernetes01"
 }
